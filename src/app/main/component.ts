5f5467bd219a6b07b7e2c55faa5385929971af0f
bynebtys <berestovyyig@gmail.com>
2019-07-25 01:39:25 +0300

    commit2

---
+++ src/app/main/component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-main',
+    templateUrl: './component.html',
+    styleUrls: ['./component.less']
+})
+export class AppMainComponent {
+    title = 'angularsite';
+}
--- src/app/root/app.module.ts
+++ src/app/root/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppHeaderComponent } from '../header/component';
+import { AppFooterComponent } from '../footer/component';
+import { AppMainComponent } from '../main/component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppHeaderComponent,
+    AppFooterComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+      AppComponent,
+      AppHeaderComponent,
+      AppFooterComponent,
+      AppMainComponent
+  ]
 })
 export class AppModule { }
